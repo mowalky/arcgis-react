b5ccb3601ed189f2659018e66b9e80578b10a8d7
Mowalky <dev@mowalky.com>
2022-07-27 20:54:31 -0500

    updated

--- src/esri/map.ts
+++ src/esri/map.ts
@@ -6,7 +6,7 @@ import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 const geoJSONLayer = new GeoJSONLayer({
-  url: "https://services3.arcgis.com/aHuST0gKxItydSpZ/ArcGIS/rest/services/CAMPUS_FACILITIES_3_6/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=",
+  url: "https://gist.githubusercontent.com/wavded/1200773/raw/e122cf709898c09758aecfef349964a8d73a83f3/sample.json",
   outFields: ["*"],
 });
 
